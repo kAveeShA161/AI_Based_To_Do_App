65259909368263b04cebbaa4fe3ccc5e3a2f85c5
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2025-12-22 01:06:04 +0530

    Routes for verifying otp added

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { login, logout, resgister } from '../controllers/authController.js';
+import { login, logout, resgister, sendVerifyOTP, verifyEmail } from '../controllers/authController.js';
+import userAuth from '../middleware/userAuth.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', resgister);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOTP);
+authRouter.post('/verify-email', userAuth, verifyEmail);
 
 export default authRouter; 
