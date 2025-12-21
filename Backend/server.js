f04a6e9f18ba6eafa8c2f0552d783ed3e8844c87
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2025-12-21 22:27:20 +0530

    Started the project

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
