e92ab2427c3146d76a92943004cb5785f559da0b
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2025-12-21 23:43:23 +0530

    function for user login and logout

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -35,7 +35,61 @@ export const resgister = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
+        return res.json({ success: true, message: "User registered successfully" });
+
     } catch (error) {
         return res.json({ success: false, message: error.message });
     }
 };
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({ success: false, message: "Email and password are required" });
+    }
+
+    try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: "Invalid email or password" });
+        }
+        
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.json({ success: false, message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign({ Id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ success: true, message: "Login successful" });
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
+
+
+export const logout = async (req, res) => {
+    try{
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+        
+        return res.json({ success: true, message: "Logout successful" });
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
