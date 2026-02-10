468bf0bdd56005cde9a4ea6aa2d885b6f323a9e3
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2026-02-10 12:04:36 +0530

    create files for pages

---
+++ frontend/src/pages/ResetPassword.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <NavBar/>
+        <h1>Reset Password Page</h1>
+    </div>
+  )
+}
+
+export default ResetPassword
