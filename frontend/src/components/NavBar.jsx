e01b5cc82cb152453a01f013d6d55b1c8fa9c0a4
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2026-02-16 10:09:48 +0530

    NavBar updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
+import { AppContext } from "../context/AppContext";
 import { assets } from "./../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { useState, useRef, useEffect } from "react";
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const NavBar = () => {
 
   const [open, setOpen] = useState(false);
   const menuRef = useRef();
 
+  const { userData, isLoggedIn, backendUrl, setUserData, setIsLoggedIn } = useContext(AppContext);
+
+  console.log("Navbar userData:", userData);
+  console.log("Navbar isLoggedIn:", isLoggedIn);
+
+
   const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + '/api/auth/logout');
+      data.success && setIsLoggedIn(false);
+      data.success && setUserData(null);
+      navigate('/')
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   useEffect(() => {
     const handler = (e) => {
-      if (!menuRef.current.contains(e.target)) {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
         setOpen(false);
       }
     };
@@ -40,40 +60,46 @@ const NavBar = () => {
 
       {/* Right Section */}
       <div className="flex items-center gap-6">
-        <button onClick={() => navigate("/login")} className="text-xl px-4 py-2 bg-red-400 text-white rounded-lg hover:bg-red-500 cursor-pointer">Login</button>
-        <button onClick={() => navigate("/register")} className="text-xl px-4 py-2 bg-red-400 text-white rounded-lg hover:bg-red-500 cursor-pointer">Register</button>
+
         <button className="text-xl px-4 py-2 bg-red-400 text-white rounded-lg hover:bg-red-500 cursor-pointer">
           + New Task
         </button>
 
-        <div className="relative flex items-center gap-3" ref={menuRef}>
-  <div className="text-right hidden sm:block">
-    <p className="font-medium text-sm">kaveeshassewmini161</p>
-    <p className="text-xs text-gray-500">
-      kaveeshassewmini161@gmail.com
-    </p>
-  </div>
+        {isLoggedIn && userData ? (
+          <div className="relative flex items-center gap-3" ref={menuRef}>
+            <div className="text-right hidden sm:block">
+              <p className="font-medium text-sm">{userData.fullName}</p>
+              <p className="text-xs text-gray-500">
+                {userData.email}
+              </p>
+            </div>
 
-  {/* Avatar */}
-  <div
-    onClick={() => setOpen(!open)}
-    className="text-xl w-10 h-10 flex items-center justify-center bg-red-400 text-white rounded-full font-semibold hover:bg-red-500 cursor-pointer"
-  >
-    KA
-  </div>
+            {/* Avatar */}
+            <div
+              onClick={() => setOpen(!open)}
+              className="text-xl w-10 h-10 flex items-center justify-center bg-red-400 text-white rounded-full font-semibold hover:bg-red-500 cursor-pointer"
+            >
+              {userData.fullName ? userData.fullName.slice(0, 2).toUpperCase() : "U"}
+            </div>
 
-  {/* Dropdown */}
-  {open && (
-    <div className="absolute right-0 top-12 w-48 bg-white rounded-2xl shadow-xl border border-gray-100 p-3 animate-fadeIn">
-      <button
-        className="text-xl flex items-center gap-2 text-red-400 font-semibold hover:bg-gray-100 w-full px-3 py-2 rounded-lg"
-        onClick={() => console.log("Sign out")}
-      >
-        Sign Out
-      </button>
-    </div>
-  )}
-</div>
+            {/* Dropdown */}
+            {open && (
+              <div className="absolute right-0 top-12 w-48 bg-white rounded-2xl shadow-xl border border-gray-100 p-3 animate-fadeIn">
+                <button
+                  className="text-xl flex items-center gap-2 text-red-400 font-semibold hover:bg-gray-100 w-full px-3 py-2 rounded-lg"
+                  onClick={logout}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            <button onClick={() => navigate("/login")} className="text-xl px-4 py-2 bg-red-400 text-white rounded-lg hover:bg-red-500 cursor-pointer">Login</button>
+            <button onClick={() => navigate("/register")} className="text-xl px-4 py-2 bg-red-400 text-white rounded-lg hover:bg-red-500 cursor-pointer">Register</button>
+          </>
+        )}
 
       </div>
     </div>
