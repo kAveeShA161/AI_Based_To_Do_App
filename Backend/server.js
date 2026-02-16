73098c04d6e68199e3640ac86c2ef8194821a23e
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2026-02-16 21:13:22 +0530

    registered the dashboardRoutes in the server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/mongodb.js';
 import authRouter from './Routes/authRoutes.js';
 import userRouter from './Routes/userRoutes.js';
 import taskRoutes from "./Routes/taskRoutes.js";
+import dashboardRoutes from "./Routes/dashboardRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -26,6 +27,8 @@ app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
 app.use("/api/task", taskRoutes);
+
+app.use("api/dashboard", dashboardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
